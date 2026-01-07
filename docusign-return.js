b0fae062fbe2f1c0ae59ae5e6d23bb657e112e75
Nishith Detroja <detroja@softsmartcorp.com>
2026-01-07 10:09:13 +0530

    fix chanes

--- docusign-return.js
+++ docusign-return.js
@@ -5,5 +5,5 @@ const envelopeId = params.get("envelopeId") || params.get("EnvelopeId");
 // Post message to parent window
 window.parent.postMessage(
   { type: "DOCUSIGN_RETURN", event, envelopeId },
-  window.location.origin
+  "*"
 );
